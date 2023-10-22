2ac741b0930fdd2f064b0461d0bc91113bab2d9d
admin <admin@DESKTOP-NNB6L08.mshome.net>
2023-10-23 00:14:15 +0530

    i have to push my code in repositories

---
+++ JavaFile/src/TexFile/FolderAction.java
@@ -0,0 +1,7 @@
+package TexFile;
+
+public class FolderAction {
+public static void main(String[] args) {
+	System.out.println("Hi shiv this is file to move to git first file");
+}
+}
---
+++ JavaFile/src/TexFile/FolderActions2.java
@@ -0,0 +1,7 @@
+package TexFile;
+
+public class FolderActions2 {
+public static void main(String[] args) {
+	System.out.println("This is sencond file");
+}
+}
